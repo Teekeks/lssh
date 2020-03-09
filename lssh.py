9eabca30cdf5e55627cd1f6462bba2680fb84aac
Lena During <info@teawork.de>
2020-03-09 11:29:46 +0100

    made it possible to pass options like -t to ssh

--- lssh.py
+++ lssh.py
@@ -6,14 +6,15 @@ import modules.mod_connect as mod_connect
 import modules.mod_profile as mod_profile
 import modules.mod_config as mod_config
 from util import show_error, init
+from sys import argv, exit
 
-argparser = argparse.ArgumentParser(description='Manage ssh connections')
-argparser.add_argument('action',
-                       metavar='action',
-                       type=str,
-                       choices=['connect', 'profile', 'config'])
-argparser.add_argument('arg', metavar='arg', type=str, nargs='*')
-args = argparser.parse_args()
+#argparser = argparse.ArgumentParser(description='Manage ssh connections')
+#argparser.add_argument('action',
+#                       metavar='action',
+#                       type=str,
+#                       choices=['connect', 'profile', 'config'])
+#argparser.add_argument('arg', metavar='arg', type=str, nargs='*')
+#args = argparser.parse_args()
 
 option_map = {
     "connect": mod_connect.handle,
@@ -21,10 +22,18 @@ option_map = {
     "config": mod_config.handle
 }
 
+if len(argv) < 2:
+    show_error("please specify action")
+    exit(1)
+action = argv[1]
+
+args = argv[2:]
+
 init()
 
-option = option_map.get(args.action)
+option = option_map.get(action)
 if option is not None:
-    option(args.arg)
+    option(args)
 else:
     show_error('unknown action')
+    exit(1)
